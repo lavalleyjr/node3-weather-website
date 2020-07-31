c73832b51704b24ed06b2240f47ad7adaf6dc66c
Justin LaValley <64431138+D41120263@users.noreply.github.com>
2020-07-31 15:41:12 -0500

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+const weatherForm = document.querySelector('form');
+const search = weatherForm.querySelector('input');
+const message1 = document.getElementById('message-1');
+const message2 = document.getElementById('message-2');
+
+weatherForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    message1.textContent = 'Loading...'
+    message2.textContent = '';
+
+    const location = search.value;
+    fetch('/weather?address=' + location).then(response => {
+        response.json().then(data => {
+            if (data.error) {
+                message1.textContent = data.error;
+            }
+            else {
+                message1.textContent = data.location;
+                message2.textContent = data.forecast;
+            }
+        })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,26 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, callback) =>
+{
+    const url = 'http://api.weatherstack.com/current?access_key=38b9a9537153a1e9bf9ea352c1047c94&query=' + latitude + ',' + longitude + '&units=f'
+
+    request({url, json: true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather service', undefined)
+        }
+        else if (body.error) {
+            callback('Unable to find location', undefined)
+        }
+        else {
+            const current = body.current
+    
+            callback(undefined, 
+                current.weather_descriptions[0] + '.' + 
+                ' It is currently ' + current.temperature +
+                ' degrees out. It feels like ' + current.feelslike + 
+                ' degrees out' )
+        }
+    })
+}
+
+module.exports = forecast
